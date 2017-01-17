162ca4d7bc7527f2919bbeaa400c44e88adade92
priyanshuch <priyanshu.chauhan@causecode.com>
2017-01-17 15:51:13 +0530

    committing changes

--- src/js/func.js
+++ src/js/func.js
@@ -1,3 +1,37 @@
 /**
  * Created by causecode on 17/1/17.
  */
+if(typeof Object.beget !== 'function'){
+	Object.beget = function(o){
+		var F=function(){};
+		F.prototype=o;
+		return new F();
+	}
+}
+
+var myObject1={
+	value: 0,
+	increment:function(inc) {
+		this.value += typeof inc === 'number' ? inc : 1;
+		adder(this);
+	},
+	add:function(){
+		this.increment();
+	}
+}
+
+function adder(obj){
+	//document.writeln(obj.name);
+	var createDiv=document.createElement('div');
+	//createDiv.innerHTML=obj.name;
+	//var value=obj.increment();
+	//document.writeln(""+obj.value);
+	createDiv.setAttribute('id','object'+obj.value);
+	createDiv.setAttribute('class','divs');
+	createDiv.innerHTML="blah blah blah";
+	document.getElementById('content').appendChild(createDiv);
+}
+
+myObject1.add();
+var myObject2=Object.beget(myObject1);
+myObject2.add();
--- src/js/inherit.js
+++ src/js/inherit.js
@@ -1,3 +1,111 @@
 /**
  * Created by causecode on 17/1/17.
  */
+function doSomething(){
+/*
+	var mammal=function(name){
+		this.name=name;
+	};
+
+	mammal.prototype.saying=function(){
+		return this.saying
+	};
+
+	mammal.prototype.add=function(){
+		this.add=adder(this);
+	};
+
+
+
+	var Cat=function(name){
+		this.name=name;
+		this.saying='meow';
+	}
+
+	Cat.prototype=new mammal();
+
+	Cat.prototype.purr=function(n){
+		var i,s='';
+		for(i=0;i<n;i++){
+			if(s){
+				s+='-';
+			}
+			s+='r';
+		}
+		return s;
+	}
+
+	var myCat=new Cat("madCat");
+	document.writeln(''+myCat.purr(5));
+	myCat.add();
+
+	function adder(obj){
+		var createDiv=document.createElement('div');
+		createDiv.setAttribute('id',''+obj.name);
+		createDiv.setAttribute('class','divs');
+		createDiv.innerHTML=''+obj.name+'_'+obj.saying;
+		document.body.appendChild(createDiv);
+	}
+*/
+var mammal=function(spec){
+	var mamm={};
+	mamm.name=spec.name;
+	mamm.saying=spec.saying||'nothing';
+	mamm.add=adder(mamm);
+	mamm.get_name=function(){
+		return spec.name;
+	}
+	return mamm;
+};
+
+	var myMamm=mammal({name:'CrazyDavid'});
+
+	var cat=function(spec){
+		spec.saying=spec.saying||'Meow!!';
+		var that = mammal(spec);
+		/*that.purr = function(n){
+			var i,s='';
+			for(i=0;i<n;i++){
+				if(s){
+					s+='-';
+				}
+				s+='r';
+			}
+			return s;
+		};*/
+		that.get_name=function(){
+			return that.saying+' '+that.name+' '+that.saying;
+		};
+		return that;
+	};
+
+	var myCat=cat({name:'Swatcat',saying:'Gonna save yall from monsters'});
+	//myCat.purr(10);
+
+	Object.prototype.superior = function(name){
+		var that=this,method=that[name];
+		return method.apply(that,arguments);
+	};
+
+	var coolcat=function(spec){
+		var that=cat(spec);
+		var super_get_name=that.superior('get_name');
+		that.get_name=function(){
+			return 'like'+super_get_name+'baby';
+		};
+		return that;
+	}
+
+	var myCoolCat=coolcat({name:'BIX'});
+	var name=myCoolCat.get_name();
+	myCoolCat.add();
+	document.body.appendChild();
+
+	function adder(obj){
+		var createDiv=document.createElement('div');
+		createDiv.setAttribute('id',''+obj.name);
+		createDiv.setAttribute('class','divs');
+		createDiv.innerHTML=''+obj.name+'_'+obj.saying;
+		document.body.appendChild(createDiv);
+	}
+}
